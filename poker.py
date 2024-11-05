ee1afdb3ea4af414b267a3bbbe0fda5de6bdd625
mystery-dog <as24973758@gmail.com>
2024-11-05 07:57:15 +0000

    setup

--- poker.py
+++ poker.py
@@ -1,7 +1,7 @@
 player1=list(input().split())
 player2=list(input().split())
 player=player1+player2
-def Error(player):
+def Error(player,player1,player2):
     for i in range(len(player1)):
         if player[i][0] not in ['A','2','3','4','5','6','7','8','9','10','J','Q','K']:
             if player[i][0]=='1' and player[i][1]=='0':
@@ -19,27 +19,58 @@ def Error(player):
             else:
                 return poker(player1,player2)
 
-def poker(player1,player2):
-    P1=pokerset(player1)
-    P2=pokerset(player2)
-
-def pokerset(player)
+def poker_number(player):
+    _poker_=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
     Ntemp=[]
+    for i in player:
+        Ntemp.append(_poker_.index(i[:-1])+1)
+    return pokerset(player,Ntemp)
+
+def poker(player1,player2):
+    P1=0
+    P2=0
+    P1=poker_number(player1)
+    P2=poker_number(player2)
+    print(max(P1,P2))
+
+def pokerset(player,Ntemp):
+    four=0
+    Three=0
+    pair=0
+    if len(set(Ntemp))==5:
+        return Straight(player,Ntemp)
+    else:
+        for i in Ntemp:
+            if Ntemp.count(i)==2:
+                pair+=1
+            if Ntemp.count(i)==3:
+                Three+=1
+            if Ntemp.count(i)==4:
+                four+=1
+        if four==1:
+            return 8
+        elif pair==2 and Three==1:
+            return 7
+        elif Three==1:
+            return 4
+        elif pair==2:
+            return 3
+        elif pair==1:
+            return 2
+
+def Straight(player,Ntemp):
+    num=[]
     Ftemp=[]
     for i in range(len(player)):
-        Ntemp.append(player([i][0]))
         Ftemp.append(player([i][1]))
-        if len(set(Ntemp))==5:
-            if len(set(Ftemp))==1:
-                return Straight_flush(player)
-            else:
-                return Straight(player)
-        elif len(set(Ntemp))==4:
-            return 1
-        elif len(set(Ntemp))==3:
-            if Ntemp 
-            for i in player:
-                if player.count(i)==2:
-                    
-Ans=Error(player)
-print(Ans)
+        num.append(Ntemp[i]-Ntemp[i-1])
+    if sorted(list(set(num))) == [1,9] and len(set(Ftemp))==1:
+        return 9
+    elif sorted(list(set(num))) == [1,9]:
+        return 5
+    elif len(set(Ftemp))==1:
+        return 6
+    else:
+        return 1
+
+Error(player,player1,player2)
